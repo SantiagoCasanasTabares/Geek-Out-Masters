e6f2ea55679c6e6bf00c050810608d21b47fc163
Santiago Casañas Tabares 202025301 <santiago.casanas@correounivalle.edu.co>
2022-01-12 10:59:33 -0500

    intentando poner el fondo en JFRAME

--- src/myProject/GUI.java
+++ src/myProject/GUI.java
@@ -2,6 +2,9 @@ package myProject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 
 /**
  * This class is used for ...
@@ -13,8 +16,10 @@ public class GUI extends JFrame  {
     private static final String AYUDA = "Aqí se explicará el juego.";
 
     private Header headerProject;
-    private ImageIcon fondoTitulo;
+    private ImageIcon fondoTitulo, imagenFondo;
     private PanelFondo fondo;
+    private JButton salir;
+    private Escucha escucha;
 
     /**
      * Constructor of GUI class
@@ -24,12 +29,16 @@ public class GUI extends JFrame  {
 
         //Default JFrame configuration
         this.setTitle("geek out");
-        this.setSize(770,535);
+        this.setSize(770,630);
         //this.pack();
         this.setResizable(false);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
 
 
     }
@@ -41,20 +50,38 @@ public class GUI extends JFrame  {
     private void initGUI() {
         //Set up JFrame Container's Layout
 
+//----------------------------------------------------------------------------------------------------------------------
+
         //Create Listener Object and Control Object
+        escucha = new Escucha();
+
+//----------------------------------------------------------------------------------------------------------------------
         //Set up JComponents
 
         //titulo
         fondoTitulo = new ImageIcon(getClass().getResource("/recursos/titulo.jpg"));
         headerProject = new Header(fondoTitulo);
-        this.add(headerProject, BorderLayout.NORTH);
-
+        this.add(headerProject, BorderLayout.NORTH); //Change this line if you change JFrame Container's Layout
 
         //fondo
-        fondo = new PanelFondo();
+       /* fondo = new PanelFondo();
         this.add(fondo, BorderLayout.CENTER);
-        this.pack();
+        this.pack();*/
 
+
+
+
+
+
+
+    }
+
+    @Override
+    public void paint(Graphics fondo) {
+        imagenFondo = new ImageIcon(getClass().getResource("/recursos/fondo.jpg"));
+        fondo.drawImage(imagenFondo.getImage(), 0, 0, getWidth(), getHeight(), this);
+
+        super.paintComponents(fondo);
     }
 
 
@@ -74,7 +101,14 @@ public class GUI extends JFrame  {
     /**
      * inner class that extends an Adapter Class or implements Listeners used by GUI class
      */
-    private class Escucha {
+    private class Escucha extends MouseAdapter implements ActionListener {
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if (e.getSource()==salir){
+                System.exit(0);
+            }
+        }
     }
 }
--- src/myProject/PanelFondo.java
+++ src/myProject/PanelFondo.java
@@ -5,17 +5,19 @@ import java.awt.*;
 
 public class PanelFondo extends JPanel {
 
+    private ImageIcon imagenFondo;
+
     public PanelFondo() {
         this.setSize(776, 571);
-
+        imagenFondo = new ImageIcon(getClass().getResource("/recursos/fondo.jpg"));
     }
+
+
 
     @Override
     public void paint(Graphics fondo){
-        Dimension tamanio = getSize();
-        ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/recursos/fondo.jpg"));
         fondo.drawImage(imagenFondo.getImage(), 0, 0, getWidth(), getHeight(), this);
-        setOpaque(false);
+        setOpaque(false);//si quito esta linea no muestra fondo
 
         super.paint(fondo);
     }
