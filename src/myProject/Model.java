40aa3f2811fa2a9c95796c270a9ced268bc2dc15
unknown <jesus.guetio@correounivalle.edu.co>
2022-01-23 15:05:14 -0500

    final model

--- src/myProject/Model.java
+++ src/myProject/Model.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Model {
     private Dado dado;
-    private boolean ganoPartida, terminoRonda;
-    private int valorCaraOpuesta, estado, puntos, ronda, estadoRonda, dadoSalvado;
+    private boolean terminoRonda;
+    private int  estado, puntos, ronda, estadoRonda, dadoSalvado, stringRonda, stringPuntos;
     private String estadoToStringRonda[], estadoToStringPuntos[];
 
     public Model(){
@@ -15,7 +15,6 @@ public class Model {
         dado=new Dado();
         estadoToStringRonda = new String[1];
         estadoToStringPuntos = new String[1];
-        ganoPartida=false;
         terminoRonda=false;
         dadoSalvado=0;
     }
@@ -28,6 +27,7 @@ public class Model {
             terminoRonda=true;
             estadoRonda=1; //la ronda termino porque solo habia dados con cara "42"
             ronda++;
+            dadoSalvado += dado.conteoCaraCuatroDos();
         }else if(dado.conteoDragones()==dado.dadosActivos.size()){
             terminoRonda=true;
             estadoRonda=2; //la ronda termino porque solo habia dados con cara "dragon"
@@ -58,18 +58,37 @@ public class Model {
     public void determinarEstado() {
         terminaRonda();
         if(ronda<5 && estadoRonda==1){
-            puntosAcumulados(dado.conteoCaraCuatroDos());
+            puntosAcumulados();
             dado.dadosActivos.clear();
             dado.dadosUsados.clear();
             dado.dadosInactivos.clear();
             tiroInicial();
+            stringPuntos=1;
+            estado=1;
+            determinarRondaToString();
         }else if((estadoRonda==2 || estadoRonda==3) && ronda<5){
             puntos=0;
+            dadoSalvado=0;
             dado.dadosActivos.clear();
             dado.dadosUsados.clear();
             dado.dadosInactivos.clear();
             tiroInicial();
-        }else if(puntos>30){}
+            stringPuntos=1;
+            estado=2;
+            determinarRondaToString();
+        }else if(ganoPartida()){
+            stringRonda=6;
+            stringPuntos=2;
+        }else if(ganoPartida()==false && ronda==5){
+            stringRonda=7;
+            stringPuntos=2;
+        }
+    }
+
+    public void determinarRondaToString() {
+        if(estado== 1 ||estado==2){
+            stringRonda=ronda;
+        }
     }
 
     public void tiroInicial(){
@@ -81,53 +100,50 @@ public class Model {
         }
     }
 
-    public int puntosAcumulados(int cuantosCuatroDos) {
-        cuantosCuatroDos = dadoSalvado;
-
-        if(cuantosCuatroDos==1) {
+    public int puntosAcumulados() {
+        if(dadoSalvado==1) {
             puntos=1;
-        }else if (cuantosCuatroDos==2) {
+        }else if (dadoSalvado==2) {
             puntos=3;
-        }else if (cuantosCuatroDos==3) {
+        }else if (dadoSalvado==3) {
             puntos=6;
-        }else if (cuantosCuatroDos==4) {
+        }else if (dadoSalvado==4) {
             puntos=10;
-        }else if (cuantosCuatroDos==5) {
+        }else if (dadoSalvado==5) {
             puntos=15;
-        }else if (cuantosCuatroDos==6) {
+        }else if (dadoSalvado==6) {
             puntos=21;
-        }else if (cuantosCuatroDos==7) {
+        }else if (dadoSalvado==7) {
             puntos=28;
-        }else if (cuantosCuatroDos==8) {
+        }else if (dadoSalvado==8) {
             puntos=36;
-        }else if (cuantosCuatroDos==9) {
+        }else if (dadoSalvado==9) {
             puntos=45;
-        }else if (cuantosCuatroDos==10) {
+        }else if (dadoSalvado==10) {
             puntos=55;
         }
         return puntos;
     }
 
-    public void dadosSalvados(){
+    /*public void dadosSalvados(){
         terminaRonda();
         if(terminoRonda==true && estadoRonda==1){
             dadoSalvado += dado.conteoCaraCuatroDos();
         }
-    }
+    }*/
 
-    /*public boolean ganoPartida(int puntaje) {
-        puntaje=puntos;
-        if (puntaje>30) {
+    public boolean ganoPartida() {
+        if (puntosAcumulados()>30) {
             return true;
         }else{
             return false;
         }
-    }*/
+    }
 
 
 
     public String[] getEstadoToStringRonda() {
-        switch (estado) {
+        switch (stringRonda) {
             case 1: estadoToStringRonda[0] = "             RONDA DE JUEGO               "+
                     "Te encuentras en la ronda 1 de Juego, aun "+
                     "tienes 4 rondas mas para lograr 30 puntos.";
@@ -161,7 +177,7 @@ public class Model {
     }
 
     public String[] getEstadoToStringPuntos() {
-        switch (estado) {
+        switch (stringPuntos) {
             case 1: estadoToStringPuntos[0] = "                      PUNTAJE                    "+
                     "Tienes un tienes un acumulado de "+puntos+" puntos.";
                 break;
